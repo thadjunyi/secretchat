14a2302ed8d6342e2ef4b74d4912be2f65982279
thadjunyi <44439239+thadjunyi@users.noreply.github.com>
2018-11-29 21:42:49 +0900

    Update chat.py

--- chat.py
+++ chat.py
@@ -112,7 +112,7 @@ def register():
 
 @app.route( '/chat/<name>/<room>', methods=['POST', 'GET'])
 def chat():
-    return render_template('./chat.html', message=room)
+    return render_template('./chat.html', keys=room)
 
 if __name__ == '__main__':
     app.secret_key = 'secretchat'
