71cf8c1d1eae3652c3f978a7fe58dcdaadf991e8
thadjunyi <44439239+thadjunyi@users.noreply.github.com>
2018-11-29 21:53:24 +0900

    Update chat.py

--- chat.py
+++ chat.py
@@ -110,9 +110,9 @@ def register():
         return render_template("./register.html", data="That username already exists!")
     return render_template('./register.html')
 
-@app.route( '/chat', methods=['POST', 'GET'])
+@app.route( '/chat/<message>', methods=['POST', 'GET'])
 def chat():
-    return render_template('./chat.html')
+    return render_template('./chat.html', message=message)
 
 if __name__ == '__main__':
     app.secret_key = 'secretchat'
