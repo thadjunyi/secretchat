fc3a26015c03ae074d344533ca35e736c4814f05
thadjunyi <44439239+thadjunyi@users.noreply.github.com>
2018-11-30 13:20:16 +0900

    Update chat.py

--- chat.py
+++ chat.py
@@ -123,4 +123,4 @@ if __name__ == '__main__':
     # app.run(debug=True)
     #app.config['SESSION_TYPE'] = 'filesystem'
     #sess.init_app(app)
-    socketio.run(app, debug = True)
+    socketio.run(app, host='https://secretchat-api-heroku.herokuapp.com', port=80, debug = True)
